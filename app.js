44aff72270c618555b12d38d430ef1d9f01616af
Gery Ruslandi <geryruslandi@gmail.com>
2022-03-18 16:03:17 +0700

    csv generation working

--- app.js
+++ app.js
@@ -5,33 +5,53 @@ const files = fs.readdirSync('./json').filter(item => !(/(^|\/)\.[^\/\.]/g).test
 for (const fileName of files) {
     const file = fs.readFileSync(`./json/${fileName}`)
     const json = JSON.parse(file);
+    const fileNameWithoutExtension = fileName.split('.').slice(0, -1).join('.');
+
+    processFile(json, fileNameWithoutExtension)
 }
 
-const formattedCsvData = {}
+function processFile(jsonObject, fileName) {
+    const formattedCsvData = {}
 
-function walkThroughObject(object, path = "") {
+    function walkThroughObject(object, path = "") {
 
-    if(object.constructor.name != 'Object') {
-        throw new Error('Only able to process Object for first level item')
+        if(object.constructor.name != 'Object') {
+            throw new Error('Only able to process Object for first level item')
+        }
+
+        for(const key in object) {
+
+            const objectType = object[key].constructor.name;
+
+            if(objectType != 'Object' && objectType != 'String') {
+                console.log(objectType)
+                throw new Error('Only able to process String or Object type for second level item')
+            }
+
+            const finalizedPath = path == '' ? key : path + `.${key}`;
+
+            if(objectType == 'String') {
+                formattedCsvData[finalizedPath] = object[key];
+            }
+
+            if(objectType == 'Object') {
+                walkThroughObject(object[key], finalizedPath)
+            }
+        }
     }
 
-    for(const key in object) {
+    walkThroughObject(jsonObject);
 
-        const objectType = object[key].constructor.name;
+    storeObjectAsCsv(formattedCsvData, fileName);
+}
 
-        if(objectType != 'Object' && objectType != 'String') {
-            console.log(objectType)
-            throw new Error('Only able to process String or Object type for second level item')
-        }
+function storeObjectAsCsv(object, fileName) {
+    // insert your language here to be
+    let csvData = 'translation_path,english'
 
-        const finalizedPath = path == '' ? key : path + `.${key}`;
-
-        if(objectType == 'String') {
-            formattedCsvData[finalizedPath] = object[key];
-        }
-
-        if(objectType == 'Object') {
-            walkThroughObject(object[key], finalizedPath)
-        }
+    for (const key in object) {
+        csvData = csvData + `\n${key},${object[key]}`
     }
+
+    fs.writeFileSync(`./csv/${fileName}.csv`, csvData)
 }
